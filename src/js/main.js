d7ffe241c93a2b2ab2d8767338dbae6153613d62
Ruslan <ruslan.postoyuk@gmail.com>
2022-01-29 20:32:30 +0200

    27. Создаем видеоплеер в модальном окне

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
 import Slider from "./modules/slider";
+import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded' , () => {
     const slider = new Slider('.page', '.next');
     slider.render();
+
+    const player = new VideoPlayer(".showup .play", ".overlay");
+/* showup - это общая секция и у кнопки есть класс play */
+    player.init(); //запустить работу плеера
+
+    /* в index html на 1017 удаляем  iframe со всеми параметрами ширины высоты и тд
+    подставляем <div id="frame"></div> потому что код который мы будем запускать в createPlayer что мы подставили
+    с верстки нам его сгенерируют автоматически c тем видео что нам понадобиться */
 });
