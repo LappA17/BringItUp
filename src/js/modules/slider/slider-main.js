52f0b1f7d63933ed7f9e2271776eecbd99d0675c
Ruslan <ruslan.postoyuk@gmail.com>
2022-01-30 17:57:45 +0200

    29. Реализация вторичных слайдеров (меньших размеров)

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,21 +1,12 @@
-/* импортируем главный слайдер что бы он служил нам как протатип, на основе его мы будем строить главный слайдер */
 import Slider from './slider';
 
-export default class MainSlider extends Slider { /* extends Slider - значит что наследуется от Слайдера, и таким образом
-мы можем получить все свойства и методы которые будут в этом классе Slider*/
+export default class MainSlider extends Slider {
 
-    constructor(page, btns) {
-        super(page, btns) /* Благодаря такой записи у нас будет два новых свойства :
-        this.page = document.querySelector(page);
-        this.btns = document.querySelectorAll(btns); 
-Эти два свойства находятся slider.js. 
-И кстати this.slides = this.page.children; тоже будет по умолчанию так как он завиит от this.page
- */
-
-/* Методы у нас не будут одинаковы в глобальных слайдерах и в маленьких. Вырезаем код с слайдера и подставляем в 
-наш глобальный ведь в самом начале мы создавали наш код для глобального слайдера, те для общего слайда и все
-эти методы ему принадлежат*/
+    /* Здесь так же удаляем page и оставляем только кнопки */
+    constructor(btns) {
+        super(btns);
     }
+
     showSlides(n) {
         if (n > this.slides.length) {
             this.slideIndex = 1;
