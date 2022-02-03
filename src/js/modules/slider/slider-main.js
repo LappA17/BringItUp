f867cab66120fe88bd29ca80333de7a7718b3519
Ruslan <ruslan.postoyuk@gmail.com>
2022-02-03 22:24:05 +0100

    33. Главный слайдер второй страницы, всплытие событий

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -41,26 +41,54 @@ export default class MainSlider extends Slider {
         this.showSlides(this.slideIndex += n);
     }
 
-    // Поместим весь рендер в try catch
+    // В самом конце урока Ваня сказал что у нас метод render сильно разрося и мы можем парочку вещей вынести
+    bindTriggers() {
+        this.btns.forEach(item => {
+            item.addEventListener('click', () => {
+                this.plusSlides(1);
+            });
+
+            item.parentNode.previousElementSibling.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.slideIndex = 1;
+                this.showSlides(this.slideIndex);
+            });
+        });
+
+        // Реализуем слайдер в нижнем меню второй страницы
+        document.querySelectorAll('.prevmodule').forEach(item => {/* all потому этих кнопочек для переключение много : на каждой страничке, на 
+на каждом модуле их много */
+            item.addEventListener("click", (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(-1); // так как мы возвращаемся на один модуль назад в слайдере то мы передадим -1
+            });
+        }); 
+
+        document.querySelectorAll('.nextmodule').forEach(item => {
+            item.addEventListener("click", (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(1); 
+            });
+        });    
+/* У нас после нижнего слайдера появился баг что после нажатия вперед перескакивает текущий слайд на два , те со 2 на 4 потом на 6.
+Здесь нужно вспомнить за всплытие событий, открываем верстку и видим что у стрелочки вперед имеет общик блок, который оборачиваем
+всю эту стрелочку и стрелочку nextmodule который мы использовали, имеет класс next этот общий модуль. И У ГЛАВНОГО СЛАЙДЕРА,
+КОТОРЫЙ СТРАНИЦЫ СЧЕЛКАЕТ ТОЖЕ ЕГО ИМЕЕТ И ПРИ КЛИКЕ НА НИЖНИЙ СЛАДЙ У НЕГО ПЛЮССЛАЙД СРАБАТЫВАЕТ ДВА РАЗА. ПО ЭТОМУ ПЕРЕДАЕМ
+ОБЪЕКТ СОБЫТИЯ ЕВЕНТ И ПЕРЕД ТЕМ КАК ЗАПУСКАТЬ ПЛЮССЛАЙДС МЫ ПРОПИШЕМ e.stopPropagation(); и так как это ссылка лучше еще
+превентдефолт добавить */
+    }
+
+    // Теперь рендер не такой большой
     render() {
-        try {
+        if (this.container) { // те если есть на странице такой элемент (не андефайнд), то произойдет код ниже
             try {
                 this.hanson = document.querySelector('.hanson');
             } catch(e){}
     
-            this.btns.forEach(item => {
-                item.addEventListener('click', () => {
-                    this.plusSlides(1);
-                });
-    
-                item.parentNode.previousElementSibling.addEventListener('click', (e) => {
-                    e.preventDefault();
-                    this.slideIndex = 1;
-                    this.showSlides(this.slideIndex);
-                });
-            });
-    
             this.showSlides(this.slideIndex);
-        } catch (e) {}
+            this.bindTriggers();
+        }    
     } 
 }
